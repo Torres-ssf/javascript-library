810bf47c548a72546d63f2a37346684b478040ec
ispirett <ispirett@gmail.com>
2019-09-13 18:55:57 -0400

    fix(stickler)

--- src/display_library.js
+++ src/display_library.js
@@ -1,35 +1,28 @@
-import library, {
-  Book,
-  AddBookToLibrary,
-  db
-} from './library.js';
+import library, { Book, AddBookToLibrary, db } from "./library";
 
 // Display logic
 
 //  Handle form
-const form = document.getElementById('form');
-const formButton = document.getElementById('button-form');
+const form = document.getElementById("form");
+const formButton = document.getElementById("button-form");
 formButton.onclick = () => {
-  console.log("form working");
-  form.style.display = 'block'
-
+    form.style.display = "block";
 };
 
 function handleForm() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const read = document.getElementById('read').value;
-  const book = new Book(title, author, pages, read);
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+    const book = new Book(title, author, pages, read);
 
-  AddBookToLibrary(book);
-  console.log(library.flat());
-  db().store('library', JSON.stringify(library.flat()));
+    AddBookToLibrary(book);
+
+    db().store("library", JSON.stringify(library.flat()));
 }
 
-const handleSubmit = document.getElementById('form');
+const handleSubmit = document.getElementById("form");
 handleSubmit.onsubmit = () => {
-
-  console.log("Testing submit button");
-  handleForm();
+    console.log("Testing submit button");
+    handleForm();
 };
