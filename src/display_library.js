7fc0a06cde40d6448645db9217c5a7a5260c06ac
Torres-ssf <sergio.uft@gmail.com>
2019-09-14 09:32:06 -0300

    feature(design): Added overlay effect when book form is active

--- src/display_library.js
+++ src/display_library.js
@@ -1,27 +1,38 @@
-import library, { Book, AddBookToLibrary, db } from "./library";
+import library, {
+  Book,
+  AddBookToLibrary,
+  db
+} from "./library";
 
 // Display logic
 
 //  Handle form
 const form = document.getElementById("form");
 const formButton = document.getElementById("button-form");
+const overlayContainer = document.querySelector(".overlay");
 formButton.onclick = () => {
-    form.style.display = "block";
+  overlayContainer.style.display = 'block';
+  form.style.display = "block";
 };
 
+overlayContainer.onclick = () => {
+  overlayContainer.style.display = 'none';
+  form.style.display = 'none';
+}
+
 function handleForm() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").value;
-    const book = new Book(title, author, pages, read);
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+  const book = new Book(title, author, pages, read);
 
-    AddBookToLibrary(book);
+  AddBookToLibrary(book);
 
-    db().store("library", JSON.stringify(library.flat()));
+  db().store("library", JSON.stringify(library.flat()));
 }
 
 const handleSubmit = document.getElementById("form");
 handleSubmit.onsubmit = () => {
-    handleForm();
+  handleForm();
 };
